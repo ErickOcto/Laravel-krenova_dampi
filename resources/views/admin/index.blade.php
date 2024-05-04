54bc0034198a69c08abbd1559038574eeb58fdcc
ErickOcto <128987328+ErickOcto@users.noreply.github.com>
2024-05-04 19:48:26 +0800

    fix chart

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -78,7 +78,7 @@
             </div>
         </div>
         <div class="row mb-4">
-            <div class="col-md-8">
+            <div class="col-md-12">
                 <div class="card">
                     <div class="card-header">
                         <h3 class='card-heading p-1 pl-3'>Project Cost</h3>
@@ -110,90 +110,75 @@
                     </div>
                 </div>
             </div>
-            <div class="col-md-4">
-                <div class="card ">
-                    <div class="card-header">
-                        <h4>Alocation</h4>
-                    </div>
-                    <div class="card-body">
-                        <div id="radialBars"></div>
-                        <div class="text-center mb-5">
-                            <h6>From last month</h6>
-                            <h1 class='text-green'>+$2,134</h1>
-                        </div>
-                    </div>
-                </div>
-                {{-- <div class="card widget-todo">
-                    <div
-                        class="card-header border-bottom d-flex justify-content-between align-items-center">
-                        <h4 class="card-title d-flex">
-                            <i class='bx bx-check font-medium-5 pl-25 pr-75'></i>Progress
-                        </h4>
-
-                    </div>
-                    <div class="card-body px-0 py-1">
-                        <table class='table table-borderless'>
-                            <tr>
-                                <td class='col-3'>UI Design</td>
-                                <td class='col-6'>
-                                    <div class="progress progress-info">
-                                        <div class="progress-bar" role="progressbar" style="width: 60%"
-                                            aria-valuenow="0" aria-valuemin="0" aria-valuemax="100">
-                                        </div>
-                                    </div>
-                                </td>
-                                <td class='col-3 text-center'>60%</td>
-                            </tr>
-                            <tr>
-                                <td class='col-3'>VueJS</td>
-                                <td class='col-6'>
-                                    <div class="progress progress-success">
-                                        <div class="progress-bar" role="progressbar" style="width: 35%"
-                                            aria-valuenow="0" aria-valuemin="0" aria-valuemax="100">
-                                        </div>
-                                    </div>
-                                </td>
-                                <td class='col-3 text-center'>30%</td>
-                            </tr>
-                            <tr>
-                                <td class='col-3'>Laravel</td>
-                                <td class='col-6'>
-                                    <div class="progress progress-danger">
-                                        <div class="progress-bar" role="progressbar" style="width: 50%"
-                                            aria-valuenow="0" aria-valuemin="0" aria-valuemax="100">
-                                        </div>
-                                    </div>
-                                </td>
-                                <td class='col-3 text-center'>50%</td>
-                            </tr>
-                            <tr>
-                                <td class='col-3'>ReactJS</td>
-                                <td class='col-6'>
-                                    <div class="progress progress-primary">
-                                        <div class="progress-bar" role="progressbar" style="width: 80%"
-                                            aria-valuenow="0" aria-valuemin="0" aria-valuemax="100">
-                                        </div>
-                                    </div>
-                                </td>
-                                <td class='col-3 text-center'>80%</td>
-                            </tr>
-                            <tr>
-                                <td class='col-3'>Go</td>
-                                <td class='col-6'>
-                                    <div class="progress progress-secondary">
-                                        <div class="progress-bar" role="progressbar" style="width: 65%"
-                                            aria-valuenow="0" aria-valuemin="0" aria-valuemax="100">
-                                        </div>
-                                    </div>
-                                </td>
-                                <td class='col-3 text-center'>65%</td>
-                            </tr>
-                        </table>
-                    </div>
-                </div> --}}
-            </div>
         </div>
     </section>
 </div>
 
 @endsection
+
+@push('add-scripts')
+<script>
+
+var chartColors = {
+  red: 'rgb(255, 99, 132)',
+  orange: 'rgb(255, 159, 64)',
+  yellow: 'rgb(255, 205, 86)',
+  green: 'rgb(75, 192, 192)',
+  info: '#41B1F9',
+  blue: '#3245D1',
+  purple: 'rgb(153, 102, 255)',
+  grey: '#EBEFF6'
+};
+
+var ctxBar = document.getElementById("bar").getContext("2d");
+var myBar = new Chart(ctxBar, {
+  type: 'bar',
+  data: {
+    labels: ["Jan", "Feb", "Mar", "Apr", "Mei", "Jun", "Jul", "Agu", "Sep", "Okt", "Nov",],
+    datasets: [{
+      label: 'Students',
+      backgroundColor: [chartColors.grey, chartColors.grey, chartColors.grey, chartColors.grey, chartColors.info, chartColors.blue, chartColors.grey],
+      data: [
+        5,
+        10,
+        30,
+        40,
+        35,
+        55,
+        15,
+      ]
+    }]
+  },
+  options: {
+    responsive: true,
+    barRoundness: 1,
+    title: {
+      display: false,
+      text: "Chart.js - Bar Chart with Rounded Tops (drawRoundedTopRectangle Method)"
+    },
+    legend: {
+      display:false
+    },
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: true,
+          suggestedMax: 40 + 20,
+          padding: 10,
+        },
+        gridLines: {
+          drawBorder: false,
+        }
+      }],
+      xAxes: [{
+            gridLines: {
+                display:false,
+                drawBorder: false
+            }
+        }]
+    }
+  }
+});
+
+</script>
+@endpush
