a7c5941a4dfe999387a9aa8813dcc21bc472667d
ErickOcto <128987328+ErickOcto@users.noreply.github.com>
2024-05-05 14:27:06 +0800

    update sidebar

--- app/Http/Controllers/RedirectController.php
+++ app/Http/Controllers/RedirectController.php
@@ -10,10 +10,14 @@ class RedirectController extends Controller
     public function redirectAfterLogin(){
         switch(Auth::user()->role){
             case 0:
+                dd(Auth::user()->name);
                 return redirect(route('admin-dashboard'));
                 break;
             case 1:
                 return redirect(route('poverty-dashboard'));
+                break;
+            case 3:
+                return redirect(route('facility.index'));
                 break;
             default:
                 return redirect(403);
--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -2,67 +2,70 @@
     <ul class="menu">
 
 
-        <li class='sidebar-title'>Main Menu</li>
-
         @if (Auth::check() && Auth::user()->role == 1)
+            <li class='sidebar-title'>Main Menu</li>
+            <li class="sidebar-item {{ request()->is('admin/poverty') ? 'active' : '' }} ">
+                <a href="{{ route('poverty-dashboard') }}" class='sidebar-link'>
+                    <i data-feather="home" width="20"></i>
+                    <span>Dashboard</span>
+                </a>
+            </li>
+        @endif
 
-        <li class="sidebar-item {{ request()->is('admin/poverty') ? 'active' : '' }} ">
-            <a href="{{ route('poverty-dashboard') }}" class='sidebar-link'>
-                <i data-feather="home" width="20"></i>
-                <span>Dashboard</span>
-            </a>
-        </li>
+        @if (Auth::check() && Auth::user()->role == 0 || Auth::user()->role == 2)
+            <li class='sidebar-title'>Main Menu</li>
+            <li class="sidebar-item {{ request()->is('admin') ? 'active' : '' }} ">
+                <a href="{{ route('admin-dashboard') }}" class='sidebar-link'>
+                    <i data-feather="home" width="20"></i>
+                    <span>Dashboard</span>
+                </a>
+            </li>
+        @endif
 
+        @if (Auth::check() && Auth::user()->role == 3 || Auth::user()->role == 0)
+            <li class='sidebar-title'>Facility Management</li>
+            <li class="sidebar-item {{ request()->is('admin/category-facility*') ? 'active' : '' }}">
+                <a href="{{ route('category-facility.index') }}" class='sidebar-link'>
+                    <i data-feather="flag" width="20"></i>
+                    <span>Facility Category</span>
+                </a>
+            </li>
+
+            <li class="sidebar-item {{ request()->is('admin/facility*') ? 'active' : '' }}">
+                <a href="{{ route('facility.index') }}" class='sidebar-link'>
+                    <i data-feather="flag" width="20"></i>
+                    <span>Facility</span>
+                </a>
+            </li>
         @endif
 
         @if (Auth::check() && Auth::user()->role == 0)
-
-        <li class="sidebar-item {{ request()->is('admin') ? 'active' : '' }} ">
-            <a href="{{ route('admin-dashboard') }}" class='sidebar-link'>
-                <i data-feather="home" width="20"></i>
-                <span>Dashboard</span>
-            </a>
-        </li>
-
-        <li class='sidebar-title'>Facility Management</li>
-
-
-        <li class="sidebar-item {{ request()->is('admin/category-facility*') ? 'active' : '' }}">
-            <a href="{{ route('category-facility.index') }}" class='sidebar-link'>
-                <i data-feather="flag" width="20"></i>
-                <span>Facility Category</span>
-            </a>
-        </li>
-
-        <li class="sidebar-item {{ request()->is('admin/facility*') ? 'active' : '' }}">
-            <a href="{{ route('facility.index') }}" class='sidebar-link'>
-                <i data-feather="flag" width="20"></i>
-                <span>Facility</span>
-            </a>
-        </li>
-
-        <li class="sidebar-item {{ request()->is('admin/user*') ? 'active' : '' }}">
-            <a href="{{ route('user.index') }}" class='sidebar-link'>
-                <i data-feather="user" width="20"></i>
-                <span>User</span>
-            </a>
-        </li>
-
-        <li class="sidebar-item {{ request()->is('admin/company*') ? 'active' : '' }}">
-            <a href="{{ route('company.index') }}" class='sidebar-link'>
-                <i data-feather="briefcase" width="20"></i>
-                <span>Company</span>
-            </a>
-        </li>
-
-        <li class="sidebar-item {{ request()->is('admin/project*') ? 'active' : '' }}">
-            <a href="{{ route('project.index') }}" class='sidebar-link'>
-                <i data-feather="trello" width="20"></i>
-                <span>Project</span>
-            </a>
-        </li>
+            <li class="sidebar-item {{ request()->is('admin/user*') ? 'active' : '' }}">
+                <a href="{{ route('user.index') }}" class='sidebar-link'>
+                    <i data-feather="user" width="20"></i>
+                    <span>User</span>
+                </a>
+            </li>
         @endif
-        @if (Auth::check() && Auth::user()->role == 1)
+
+        @if (Auth::check() && Auth::user()->role == 2 || Auth::user()->role == 0)
+            <li class='sidebar-title'>Project Management</li>
+            <li class="sidebar-item {{ request()->is('admin/company*') ? 'active' : '' }}">
+                <a href="{{ route('company.index') }}" class='sidebar-link'>
+                    <i data-feather="briefcase" width="20"></i>
+                    <span>Company</span>
+                </a>
+            </li>
+
+            <li class="sidebar-item {{ request()->is('admin/project*') ? 'active' : '' }}">
+                <a href="{{ route('project.index') }}" class='sidebar-link'>
+                    <i data-feather="trello" width="20"></i>
+                    <span>Project</span>
+                </a>
+            </li>
+        @endif
+
+        @if (Auth::check() && Auth::user()->role == 1 || Auth::user()->role == 0)
             <li class='sidebar-title'>Managemen Penduduk</li>
 
             <li class="sidebar-item {{ request()->is('admin/management/poverty*') ? 'active' : '' }}">
